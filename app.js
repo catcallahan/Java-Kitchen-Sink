57cec0372838c58c96d389337d0999d95b0f6ada
student <student@Admins-iMac.local>
2020-01-14 12:48:40 -0600

    formatted code

--- app.js
+++ app.js
@@ -3,10 +3,10 @@ const statesUSA = 50;
 var addedNum = 5 + 4;
 /* created  variables for my name, the sum of 5 +4 and the number of states in the USA */
 
-function sayHello(greeting){
+function sayHello(greeting) {
     alert(greeting)
 }
-sayHello(greeting ="Hello World!")
+sayHello(greeting = "Hello World!")
 
 
 function checkAge(name, age) {
@@ -22,22 +22,37 @@ checkAge((name = "John"), 17);
 
 var favVeg = ['Carrots', 'Asparagus', 'Corn'];
 
-for(var i = 0; i < favVeg.length; i++) {
+for (var i = 0; i < favVeg.length; i++) {
     console.log(favVeg[i]);
 };
 
-var friends = [ (Cat = {name: "Cat", age: 30}), (John = {name: "John", age: 29}), Mollie = {name: "Mollie", age: 19}, David = {name: 'David', age: 14}, Brad = {name: 'Brad', age: 30}]
+var friends = [(Cat = {
+    name: "Cat",
+    age: 30
+}), (John = {
+    name: "John",
+    age: 29
+}), Mollie = {
+    name: "Mollie",
+    age: 19
+}, David = {
+    name: 'David',
+    age: 14
+}, Brad = {
+    name: 'Brad',
+    age: 30
+}]
 
-for(var i = 0; i < friends.length; i++) {
+for (var i = 0; i < friends.length; i++) {
     checkAge(friends[i].name, friends[i].age)
 }
 
 function getLength(word) {
-        if (word.length%2 == 0) {
-            console.log('The World is nice and even!')
-} else { 
-    console.log('The world is an odd place!')
+    if (word.length % 2 == 0) {
+        console.log('The World is nice and even!')
+    } else {
+        console.log('The world is an odd place!')
     }
-    
+
 }
 getLength(word = 'Hello World!')
